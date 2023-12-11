709d7a3220b13216c296c3a00be0747e932bcdbe
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2023-12-11 18:02:21 +0100

    PokemonCard component edited: now it takes into account imgSrc

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,28 @@
 function PokemonCard() {
+    const pokemon = pokemonList;
     return (
-        <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon-card" /><figcaption>Bulbasaur</figcaption></figure>
-        );
+    <>
+        {pokemon.map((element) => {
+            return element.imgSrc !== undefined ? (
+            <figure><img src={element.imgSrc} alt={element.name + " card"} /><figcaption>{element.name}</figcaption></figure>
+            ) 
+            : (
+            <p> ??? <figcaption>{element.name}</figcaption></p>
+            )
+        })}
+    </>
+    )
 }
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 export default PokemonCard;
 
