1f0865f78021a3ae6e89a6860e4bdf771f69d127
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2023-12-12 11:24:01 +0100

    exo 04 - Pokédex-Passer des props : done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,28 +1,11 @@
-function PokemonCard() {
-    const pokemon = pokemonList;
+function PokemonCard({pokemon}) {
+  // console.log(pokemon);
     return (
     <>
-        {pokemon.map((element) => {
-            return element.imgSrc !== undefined ? (
-            <figure><img src={element.imgSrc} alt={element.name + " card"} /><figcaption>{element.name}</figcaption></figure>
-            ) 
-            : (
-            <p> ??? <figcaption>{element.name}</figcaption></p>
-            )
-        })}
+      <figure><img src = {pokemon.imgSrc} alt = {pokemon.name + " card"} /><figcaption>{pokemon.name}</figcaption></figure>     
     </>
     )
 }
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 export default PokemonCard;
 
